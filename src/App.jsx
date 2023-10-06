c36dcba3084a1b999fd5333652ce67a60fe2a741
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-06 20:50:58 +0530

    added omdb api key, routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './App.scss'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app'>
+      <BrowserRouter>
+        <Header></Header>
+
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/movie/:imdbID' element={<MovieDetails />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKEY = "9cf71097";
