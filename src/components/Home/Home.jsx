53ca9b29fea0a5c0b0314107d7680454385373c7
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-06 22:29:41 +0530

    axios for the api call

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,22 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing';
-
+import movieApi from '../../apis/movieApi';
+import { APIKEY } from '../../apis/MovieApiKey';
 const Home = () => {
+
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async() => {
+      const response = await movieApi
+      .get(`?apiKey=${APIKEY}&s=${movieText}&type=movie`)
+      .catch((err) => {
+        console.log("Error: ", err.message);
+      });
+      console.log("The response from API ", response);
+    }
+
+    fetchMovies();
+  }, []);
   return (
     <div>
       <div className='banner-img'></div>
