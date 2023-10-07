aeb293afa30d1d5815c9e7808468cc71c7881ddc
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-07 11:37:32 +0530

    footer align bottom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,7 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <div className='footer'>
-      <div>Redux Movies</div>
-      <div>@2023 ReduxMovies, Inc. or its affiliates</div>
+      <div>&copy; ReduxMovies | 2023</div>
     </div>
   )
 }
