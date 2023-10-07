f70fec669d8af7c742a472d5c84df41741f9a460
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-07 08:58:03 +0530

    dispatch, selector, movie listing component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,21 @@ import { Fragment, useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing';
 import movieApi from '../../apis/movieApi';
 import { APIKEY } from '../../apis/MovieApiKey';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 const Home = () => {
 
+  const dispatch = useDispatch();
+  const movieText = "Harry";
+
   useEffect(() => {
-    const movieText = "Harry";
     const fetchMovies = async() => {
       const response = await movieApi
       .get(`?apiKey=${APIKEY}&s=${movieText}&type=movie`)
       .catch((err) => {
         console.log("Error: ", err.message);
       });
+      dispatch(addMovies(response.data))
       console.log("The response from API ", response);
     }
 
